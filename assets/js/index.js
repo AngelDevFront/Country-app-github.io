fb5b8df7ddae3e3aafc9e15e76a497cba44d8ef7
david fontaine <devilangel6089@gmail.comgit config --global user.name AngelDevil96>
2024-04-04 09:42:42 +0200

    projet fini

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 const countriesContainer = document.querySelector(".countries-container");
+const btnSort = document.querySelectorAll(".btnSort");
 // faire apparaître les données dans une variable
 let countriesData = [];
+let sortMethod = "maxToMin";
 //créez une fonction
 async function fetchCountries() {
   // fait apparaître les données de l'url dans la console
@@ -16,6 +18,26 @@ async function fetchCountries() {
 //créez une fonction d'affichage et paramétrer l'affichage des cartes de chaque pays grace à la méthode map
 function countriesDisplay() {
   countriesContainer.innerHTML = countriesData
+    // Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
+    .filter((country) =>
+      country.translations.fra.common
+        .toLowerCase()
+        .includes(inputSearch.value.toLowerCase())
+    )
+    // Gérer les 3 boutons pour trier (méthode sort()) les pays
+    .sort((a, b) => {
+      if (sortMethod === "maxToMin") {
+        return b.population - a.population;
+      } else if (sortMethod === "minToMax") {
+        return a.population - b.population;
+      } else if (sortMethod === "alpha") {
+        return a.translations.fra.common.localeCompare(
+          b.translations.fra.common
+        );
+      }
+    })
+    // Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
+    .slice(0, inputRange.value)
     .map(
       (country) =>
         `
@@ -32,3 +54,15 @@ function countriesDisplay() {
     .join("");
 }
 window.addEventListener("load", fetchCountries);
+inputSearch.addEventListener("input", countriesDisplay);
+inputRange.addEventListener("input", () => {
+  countriesDisplay;
+  rangeValue.textContent = inputRange.value; // va connecter les 2 élément
+});
+
+btnSort.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    sortMethod = e.target.id;
+    countriesDisplay();
+  });
+});
